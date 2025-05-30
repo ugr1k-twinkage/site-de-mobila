2e1018bc49a0404d81a2f011b995b6313aa8e9fd
Igor Spatari <igorkaplay54@gmail.com>
2025-05-30 19:13:31 +0300

    am adaugat un footere mai stilizat si am facut un update la imagini

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,14 +1,15 @@
 let a = 5;
 let b = 10;
 suma = a + b;
-
 console.log("Suma numerelor este: ", suma);
+
 for(let i =1; i <= 10; i++){
     if (i % 2 == 0){
         console.log(i);
     }
 
 }
+
 let name = prompt("Introduceti numele si prenumele dvs.:") 
 console.log(name)
 alert("Salut " + name +"!")
